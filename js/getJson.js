86cfde51dcd1c90182af3e63de4bc479ad250e6e
michiyuki68 <michiyuki.k68@gmail.com>
2017-03-14 23:18:46 +0900

    i

---
+++ js/getJson.js
@@ -0,0 +1,20 @@
+$(function() {
+  $.getJSON("data.json", function(data) {
+
+    var ulObj1 = $("#Headlines"),
+      ulObj2 = $("#Bodies"),
+      len = data.length;
+
+    for (var i = 0; i < len; i++) {
+      var firstBodyPreview, limitNo;
+      limitNo = 100;
+
+
+      ulObj1.append($('<div class="swiper-slide">' + data[i].FirstBody + '</div>'));
+
+    }
+
+
+  });
+});
+
