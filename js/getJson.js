822c5042d5432e9ecb03eb72535345946e7fe205
michiyuki68 <michiyuki.k68@gmail.com>
2017-03-15 00:16:42 +0900

    s

--- js/getJson.js
+++ js/getJson.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function() {
   $.getJSON("data.json", function(data) {
 
@@ -18,7 +17,3 @@ $(function() {
 
   });
 });
-
-=======
-$(function() {  $.getJSON("data.json", function(data) {    var ulObj1 = $("#Headlines"),      ulObj2 = $("#Bodies"),      len = data.length;    for (var i = 0; i < len; i++) {      var firstBodyPreview, limitNo;      limitNo = 100;      ulObj1.append($('<div class="swiper-slide">' + data[i].FirstBody + '</div>'));    }  });});
->>>>>>> origin/master
