d04420eb5ca2d74e11733e146856a8bd0cb80750
michiyuki68 <michiyuki.k68@gmail.com>
2017-03-17 01:35:40 +0900

    hh

--- js/getJson.js
+++ js/getJson.js
@@ -3,16 +3,33 @@ $(function() {
 
     var ulObj1 = $("#Headlines"),
       ulObj2 = $("#Bodies"),
-      len = data.length;
+      len = data.length,
+      strCtgr = '',
+      strPre = '<div class="swiper-container"><div class="swiper-wrapper">',
+      strAft = '</div><div class="swiper-pagination"></div><div class="swiper-button-next"></div><div class="swiper-button-prev"></div></div>';
 
     for (var i = 0; i < len; i++) {
-      var firstBodyPreview, limitNo;
-      limitNo = 100;
 
 
-      ulObj1.append($('<div class="swiper-slide">' + data[i].FirstBody + '</div>'));
+//Category‚ª•Ï‚í‚Á‚½‚çdiv
+if (data[i].Category == strCtgr){
+  if (strCtgr != ''){
+    ulObj1.append($(strAft));
+  }
+  strCtgr = data[i].Category;
+  //ƒ‰ƒXƒg‚Í‚â‚ç‚È‚¢
+  ulObj1.append($(strPre));
+}
 
-    }
+//if (data[i].Category == "ˆê–Ê"){
+      ulObj1.append($('<div class="swiper-slide">' + data[i].Category + data[i].FirstBody + '</div>'));
+//    }
+
+//      ulObj1.append($(strPre + data[i].Category + data[i].FirstBody + '</div>'));
+
+     }
+
+    ulObj1.append($(strAft));
 
 
   });
