02b53ca8e6728b30503e900037f01c04bb6f0bca
michiyuki68 <michiyuki.k68@gmail.com>
2017-03-17 20:53:45 +0900

    s

--- js/getJson.js
+++ js/getJson.js
@@ -1,42 +1,39 @@
-$(function() {
-  $.getJSON("data.json", function(data) {
+$(function () {
+    $.getJSON("data.json", function (data) {
 
-    var ulObj1 = $("#Headlines"),
-      ulObj2 = $("#Bodies"),
-      len = data.length,
-      strCtgr = '',
-      strPre = '<div class="swiper-container"><div class="swiper-wrapper">',
-      strAft = '</div><div class="swiper-pagination"></div><div class="swiper-button-next"></div><div class="swiper-button-prev"></div></div>';
+        var ulObj1 = $("#Headlines"),
+          ulObj2 = $("#Bodies"),
+          len = data.length,
+          strCtgr = '',
+          strPre = '<div class="swiper-container"><div class="swiper-wrapper">',
+          strAft = '</div><div class="swiper-pagination"></div><div class="swiper-button-next"></div><div class="swiper-button-prev"></div></div>';
 
-    for (var i = 0; i < len; i++) {
-console.log('before if');
-console.log(data[i].Category);
-console.log(strCtgr);
+        for (var i = 0; i < len; i++) {
+            //初回のみ必要
+            if (strCtgr == '') {
+                ulObj1.append($(strPre));
+            }
 
-//Categoryが変わったらdiv
-if (data[i].Category == strCtgr){
-console.log("after if");
-  if (strCtgr != ''){
-console.log(strCtgr);
-    ulObj1.append($(strAft));
-  }
+            console.log('before if');
+            console.log(data[i].Category);
+            console.log(strCtgr);
 
-  strCtgr = data[i].Category;
-console.log('dainyugo:' + strCtgr);
-  //ラストはやらない
-  ulObj1.append($(strPre));
-}
+            //Categoryが変わったらdiv
+            if (data[i].Category == strCtgr) {
+                console.log("kawatta if");
+                ulObj1.append($(strAft));
+                ulObj1.append($(strPre));
+            }
 
-//if (data[i].Category == "一面"){
-      ulObj1.append($('<div class="swiper-slide">' + 'a' + data[i].Category + data[i].FirstBody + '</div>'));
-//    }
+            strCtgr = data[i].Category; //毎回退避しておく
+            console.log('dainyugo:' + strCtgr);
 
-//      ulObj1.append($(strPre + data[i].Category + data[i].FirstBody + '</div>'));
+            ulObj1.append($('<div class="swiper-slide">' + 'b' + data[i].Category + data[i].FirstBody + '</div>'));
 
-     }
-
-    ulObj1.append($(strAft));
+        }
+        //最後に必要
+        ulObj1.append($(strAft));
 
 
-  });
+    });
 });
